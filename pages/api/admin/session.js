c3eec738d1b69c84ced863e297e0f14562dfc82a
Nitish R Honnali <nitishrhonnali@gmail.com>
2026-09-15 23:08:21 +0530

    chore: remove remaining legacy pages dir

--- pages/api/admin/session.js
+++
@@ -1,9 +0,0 @@
-const { isAdminAuthorized } = require("../../../lib/admin-auth");
-
-export default async function handler(req, res) {
-  if (req.method !== "GET") {
-    return res.status(405).json({ error: "Method not allowed" });
-  }
-
-  return res.status(200).json({ admin: isAdminAuthorized(req) });
-}
